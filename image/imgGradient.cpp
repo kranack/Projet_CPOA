641dd71a67295cfe68329cc6e4e9c848faf08359
dcalesse <dcalesse@pcj420.psi.ad.unistra.fr>
2014-11-27 16:17:05 +0100

    debug this shit

--- image/imgGradient.cpp
+++ image/imgGradient.cpp
@@ -2,7 +2,7 @@
 
 
 
-const int GradientSobel::Sobel5x[25]={1,2,0,-2,-1,
+/*const int GradientSobel::Sobel5x[25]={1,2,0,-2,-1,
 									  4,8,0,-8,-4,
 									  6,12,0,-12,-6,
 									  4,8,0,-8,-4,
@@ -24,5 +24,5 @@ Image2<Vec2f> GradientSobel::gradient33(const Image2grey& img_in)
 Image2<Vec2f> GradientSobel::gradient55(const Image2grey& img_in)
 {
 }
-
+*/
 
